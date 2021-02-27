4f150c7f1eeb9c55ac1b619016d5b50b801ac2d2
Alderbane <51372365+Alderbane@users.noreply.github.com>
2021-02-26 18:43:45 -0600

    created event page file

---
+++ lib/event_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EventPage extends StatefulWidget {
+  EventPage({Key key}) : super(key: key);
+
+  @override
+  _EventPageState createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("s"),
+    );
+  }
+}
