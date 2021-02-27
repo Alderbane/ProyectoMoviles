c242d074d37a2c0dc49839af9043953d6f5be7b1
Alderbane <51372365+Alderbane@users.noreply.github.com>
2021-02-26 19:17:15 -0600

    Modelo de evento

---
+++ lib/models/event_model.dart
@@ -0,0 +1,13 @@
+class Evento {
+  DateTime fecha;
+  String titulo;
+  String descripcion;
+  String hora;
+
+  Evento({
+    this.fecha,
+    this.titulo,
+    this.descripcion,
+    this.hora,
+  }) {}
+}
